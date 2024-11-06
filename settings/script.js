8c22c6d699932ed9f6207905591164ead626ae21
Anes Bengasmia <37506264+anesnengasmia@users.noreply.github.com>
2024-11-06 19:58:45 +0100

    push

---
+++ settings/script.js
@@ -0,0 +1,15 @@
+const X1 = document.getElementById("x1").querySelector('input');
+const X2 = document.getElementById("x2").querySelector('input');
+const X3 = document.getElementById("x3").querySelector('input');
+const Y1 = document.getElementById("y1").querySelector('input');
+const Y2 = document.getElementById("y2").querySelector('input');
+const Y3 = document.getElementById("y3").querySelector('input');
+const Z1 = document.getElementById("z1").querySelector('input');
+const Z2 = document.getElementById("z2").querySelector('input');
+const Z3 = document.getElementById("z3").querySelector('input');
+
+var X = parseFloat(X1.value) + parseFloat(X2.value) + parseFloat(X3.value);
+var Y = parseFloat(Y1.value) + parseFloat(Y2.value) + parseFloat(Y3.value);
+var Z = parseFloat(Z1.value) + parseFloat(Z2.value) + parseFloat(Z3.value);
+
+alert("Sum of X: " + X + "\nSum of Y: " + Y + "\nSum of Z: " + Z);

92f532d0dd0494884647b06be6873f16924fb4d2
Anes Bengasmia <37506264+anesnengasmia@users.noreply.github.com>
2024-11-07 12:21:18 +0100

    update

--- settings/script.js
+++ settings/script.js
@@ -1,20 +1,23 @@
-const X1 = document.getElementById("x1").querySelector('input');
-const X2 = document.getElementById("x2").querySelector('input');
-const X3 = document.getElementById("x3").querySelector('input');
-const Y1 = document.getElementById("y1").querySelector('input');
-const Y2 = document.getElementById("y2").querySelector('input');
-const Y3 = document.getElementById("y3").querySelector('input');
-const Z1 = document.getElementById("z1").querySelector('input');
-const Z2 = document.getElementById("z2").querySelector('input');
-const Z3 = document.getElementById("z3").querySelector('input');
+const M11 = document.getElementById("x1").querySelector('input');
+const M12 = document.getElementById("x2").querySelector('input');
+const M13 = document.getElementById("x3").querySelector('input');
+const M21 = document.getElementById("y1").querySelector('input');
+const M22 = document.getElementById("y2").querySelector('input');
+const M23 = document.getElementById("y3").querySelector('input');
+const M31 = document.getElementById("z1").querySelector('input');
+const M32 = document.getElementById("z2").querySelector('input');
+const M33 = document.getElementById("z3").querySelector('input');
 
-var X = parseFloat(X1.value) + parseFloat(X2.value) + parseFloat(X3.value);
-var Y = parseFloat(Y1.value) + parseFloat(Y2.value) + parseFloat(Y3.value);
-var Z = parseFloat(Z1.value) + parseFloat(Z2.value) + parseFloat(Z3.value);
+
 
 function openSoulutionPage() {
     window.location.href = './functions/solution.html'; 
 }
 function openMainPage() {
     window.location.href = '../'; 
+}
+function showSoulutiosn() {
+    var X = document.getElementById("X");
+    var Y = document.getElementById("Y");
+    var Z = document.getElementById("Z");
 }
